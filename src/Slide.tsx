d958c90f390e10e02eb8b03cd49f897c2b600518
kaidge <kj@dfrnt.world>
2024-10-17 20:56:08 -0700

    one last hoorah

--- src/Slide.tsx
+++ src/Slide.tsx
@@ -13,24 +13,38 @@ import BAS from "./utils/BAS";
 
 const SlideShaderMaterial = shaderMaterial(
   {
-    uProgress: 0,
+    uTime: 0,
     map: new THREE.Texture(),
   },
   // vertex shader
   `
-    uniform float uProgress;
-    attribute float aProgress;
+    uniform float uTime;
+    attribute vec2 aAnimation;
     attribute vec3 aStartPosition;
+    attribute vec3 aControl0;
+    attribute vec3 aControl1;
     attribute vec3 aEndPosition;
 
-    varying float vProgress;
     varying vec2 vUv;
 
+    vec3 cubicBezier(vec3 p0, vec3 c0, vec3 c1, vec3 p1, float t) {
+      float tn = 1.0 - t;
+      return tn * tn * tn * p0 + 3.0 * tn * tn * t * c0 + 3.0 * tn * t * t * c1 + t * t * t * p1;
+    }
+
+    float easeInOutCubic(float t) {
+      return t < 0.5 ? 4.0 * t * t * t : 1.0 - pow(-2.0 * t + 2.0, 3.0) / 2.0;
+    }
+
     void main() {
-      vProgress = aProgress;
       vUv = uv;
       
-      vec3 newPosition = mix(aStartPosition, aEndPosition, step(aProgress, uProgress));
+      float tDelay = aAnimation.x;
+      float tDuration = aAnimation.y;
+      float tTime = clamp(uTime - tDelay, 0.0, tDuration);
+      float tProgress = easeInOutCubic(tTime / tDuration);
+
+      vec3 newPosition = cubicBezier(aStartPosition, aControl0, aControl1, aEndPosition, tProgress);
       
       gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
     }
@@ -38,17 +52,11 @@ const SlideShaderMaterial = shaderMaterial(
   // fragment shader
   `
     uniform sampler2D map;
-    uniform float uProgress;
-    varying float vProgress;
     varying vec2 vUv;
 
     void main() {
       vec4 texColor = texture2D(map, vUv);
-      
-      // Blend between original color and white based on progress
-      vec3 color = mix(texColor.rgb, vec3(1.0), step(vProgress, uProgress));
-      
-      gl_FragColor = vec4(color, 1.0);
+      gl_FragColor = texColor;
     }
   `
 );
@@ -70,15 +78,22 @@ interface SlideProps {
   animationPhase: "in" | "out";
 }
 
-function getRandomEndPosition(
-  startPosition: THREE.Vector3,
-  maxDistance: number
-) {
-  const endPosition = startPosition.clone();
-  endPosition.x += THREE.MathUtils.randFloatSpread(maxDistance);
-  endPosition.y += THREE.MathUtils.randFloatSpread(maxDistance);
-  endPosition.z += THREE.MathUtils.randFloatSpread(maxDistance / 2);
-  return endPosition;
+function getControlPoint0(centroid: THREE.Vector3) {
+  const signY = Math.sign(centroid.y);
+  return new THREE.Vector3(
+    THREE.MathUtils.randFloat(0.1, 0.3) * 50,
+    signY * THREE.MathUtils.randFloat(0.1, 0.3) * 70,
+    THREE.MathUtils.randFloatSpread(20)
+  );
+}
+
+function getControlPoint1(centroid: THREE.Vector3) {
+  const signY = Math.sign(centroid.y);
+  return new THREE.Vector3(
+    THREE.MathUtils.randFloat(0.3, 0.6) * 50,
+    -signY * THREE.MathUtils.randFloat(0.3, 0.6) * 70,
+    THREE.MathUtils.randFloatSpread(20)
+  );
 }
 
 const Slide = forwardRef<THREE.Mesh, SlideProps>(
@@ -89,43 +104,116 @@ const Slide = forwardRef<THREE.Mesh, SlideProps>(
     useImperativeHandle(ref, () => meshRef.current as THREE.Mesh, []);
 
     const geometry = useMemo(() => {
-      const plane = new THREE.PlaneGeometry(width, height, width, height);
+      const plane = new THREE.PlaneGeometry(
+        width,
+        height,
+        width * 2,
+        height * 2
+      );
+      BAS.Utils.separateFaces(plane);
 
-      const aProgress = new Float32Array(plane.attributes.position.count);
+      const aAnimation = new Float32Array(plane.attributes.position.count * 2);
       const aStartPosition = new Float32Array(
         plane.attributes.position.count * 3
       );
+      const aControl0 = new Float32Array(plane.attributes.position.count * 3);
+      const aControl1 = new Float32Array(plane.attributes.position.count * 3);
       const aEndPosition = new Float32Array(
         plane.attributes.position.count * 3
       );
 
-      for (let i = 0; i < plane.attributes.position.count; i++) {
-        const startPosition = new THREE.Vector3().fromBufferAttribute(
-          plane.attributes.position,
-          i
-        );
-        const endPosition = getRandomEndPosition(startPosition, 20);
+      const minDuration = 0.8;
+      const maxDuration = 1.2;
+      const maxDelayX = 0.9;
+      const maxDelayY = 0.125;
+      const stretch = 0.11;
 
-        aProgress[i] = Math.random();
-        aStartPosition.set(
-          [startPosition.x, startPosition.y, startPosition.z],
-          i * 3
+      const startPosition = new THREE.Vector3();
+      const control0 = new THREE.Vector3();
+      const control1 = new THREE.Vector3();
+      const endPosition = new THREE.Vector3();
+
+      for (let i = 0; i < plane.attributes.position.count; i += 3) {
+        const face = Math.floor(i / 3);
+        const centroid = BAS.Utils.computeCentroid(plane, face);
+
+        // animation
+        const duration = THREE.MathUtils.randFloat(minDuration, maxDuration);
+        const delayX = THREE.MathUtils.mapLinear(
+          centroid.x,
+          -width * 0.5,
+          width * 0.5,
+          0.0,
+          maxDelayX
         );
-        aEndPosition.set([endPosition.x, endPosition.y, endPosition.z], i * 3);
+        const delayY =
+          animationPhase === "in"
+            ? THREE.MathUtils.mapLinear(
+                Math.abs(centroid.y),
+                0,
+                height * 0.5,
+                0.0,
+                maxDelayY
+              )
+            : THREE.MathUtils.mapLinear(
+                Math.abs(centroid.y),
+                0,
+                height * 0.5,
+                maxDelayY,
+                0.0
+              );
+
+        for (let v = 0; v < 3; v++) {
+          const vIndex = i + v;
+          aAnimation[vIndex * 2] =
+            delayX + delayY + Math.random() * stretch * duration;
+          aAnimation[vIndex * 2 + 1] = duration;
+        }
+
+        // positions
+        endPosition.copy(centroid);
+        startPosition.copy(centroid);
+
+        if (animationPhase === "in") {
+          control0.copy(centroid).sub(getControlPoint0(centroid));
+          control1.copy(centroid).sub(getControlPoint1(centroid));
+        } else {
+          control0.copy(centroid).add(getControlPoint0(centroid));
+          control1.copy(centroid).add(getControlPoint1(centroid));
+        }
+
+        for (let v = 0; v < 3; v++) {
+          const vIndex = i + v;
+          aStartPosition.set(
+            [startPosition.x, startPosition.y, startPosition.z],
+            vIndex * 3
+          );
+          aControl0.set([control0.x, control0.y, control0.z], vIndex * 3);
+          aControl1.set([control1.x, control1.y, control1.z], vIndex * 3);
+          aEndPosition.set(
+            [endPosition.x, endPosition.y, endPosition.z],
+            vIndex * 3
+          );
+        }
       }
 
-      plane.setAttribute("aProgress", new THREE.BufferAttribute(aProgress, 1));
+      plane.setAttribute(
+        "aAnimation",
+        new THREE.BufferAttribute(aAnimation, 2)
+      );
       plane.setAttribute(
         "aStartPosition",
         new THREE.BufferAttribute(aStartPosition, 3)
       );
+      plane.setAttribute("aControl0", new THREE.BufferAttribute(aControl0, 3));
+      plane.setAttribute("aControl1", new THREE.BufferAttribute(aControl1, 3));
       plane.setAttribute(
         "aEndPosition",
         new THREE.BufferAttribute(aEndPosition, 3)
       );
 
       return plane;
-    }, [width, height]);
+    }, [width, height, animationPhase]);
 
     const texture = useMemo(
       () => new THREE.TextureLoader().load(imageUrl),
@@ -136,12 +224,12 @@ const Slide = forwardRef<THREE.Mesh, SlideProps>(
       if (materialRef.current) {
         const duration = 3.0;
         gsap.fromTo(
-          materialRef.current.uniforms.uProgress,
+          materialRef.current.uniforms.uTime,
           { value: 0 },
           {
-            value: 1,
+            value: duration,
             duration: duration,
-            ease: "power1.inOut",
+            ease: "power0.inOut",
             repeat: -1,
             yoyo: true,
           }
@@ -155,7 +243,7 @@ const Slide = forwardRef<THREE.Mesh, SlideProps>(
           ref={materialRef}
           map={texture}
           transparent
-          uniforms-uProgress-value={0}
+          uniforms-uTime-value={0}
         />
       </mesh>
     );
